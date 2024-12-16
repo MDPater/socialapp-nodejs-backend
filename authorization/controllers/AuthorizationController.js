0176b6fa0051e96d58fc612272370d49e3919b0b
Max de Pater <depatermax@gmail.com>
2024-12-17 00:27:50 +0100

    login function

--- authorization/controllers/AuthorizationController.js
+++ authorization/controllers/AuthorizationController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const pool = require('../../db');
 const queries = require('./queries');
+const { error } = require('console');
 
 const jwtSecret = process.env.JWTSECRET;
 const jwtExpirationInSeconds = process.env.JWT_EXPIRATION_IN_SECONDS;
@@ -45,7 +46,6 @@ module.exports = {
             if(userExists.rows.length > 0){
                 return res.status(400).json({error: "Username/email already exists"})
             }
-            
             //register user in db and create accessToken
             const newUser = await pool.query(queries.register, [username, email, encryptedPassword]);
             const userId = newUser.rows[0].id;
@@ -62,12 +62,40 @@ module.exports = {
     },
 
     //login User
-    login: (req, res) => {
+    login: async (req, res) => {
         const {username, password} = req.body;
 
-        //check if username exists
+        try{
+            //check if username exists
+            const checkUser = await pool.query(queries.checkUser, [username])
 
-        //
+            if(checkUser.rows.length === 0){
+                return res.status(400).json({
+                    error: "User does not exist"
+                })
+            }
+
+            //compare hashed passwords
+            let encryptedPassword = encryptPassword(password);
+            if(checkUser.rows[0].password !== encryptedPassword){
+                return res.status(400).json({
+                    error: "Password not Valid"
+                })
+            }
+
+            //generate token
+            const userId = checkUser.rows[0].id;
+            const accessToken = generateAccessToken(username, userId);
+
+            res.status(201).json({status: true, user: checkUser.rows[0], token: accessToken})
+
+        }catch(e){
+            console.log(e.message);
+            res.status(500).json({
+                error: "Internal Server Error"
+            })
+        }
+
         
     }
 }
